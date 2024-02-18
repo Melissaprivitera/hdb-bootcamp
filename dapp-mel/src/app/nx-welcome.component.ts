1548c3e2826302d3ca0c4cecbc572d3b28504921
Melissa Privitera <melissa.privitera@protonmail.com>
2024-02-18 21:33:37 +0100

    last commit

--- dapp-mel/src/app/nx-welcome.component.ts
+++ dapp-mel/src/app/nx-welcome.component.ts
@@ -432,7 +432,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome dapp-mel 👋
+            Welcome My fist Angular page 👋
           </h1>
         </div>
         <!--  HERO  -->
